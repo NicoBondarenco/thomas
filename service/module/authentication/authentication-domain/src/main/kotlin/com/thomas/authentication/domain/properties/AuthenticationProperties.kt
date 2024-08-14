6dacd0d79a37a858cfe80bd37a5f92efb64433a2
Nico Bondarenco <nicanor_bondarenco@hotmail.com>
2024-08-14 12:54:54 -0300

    Implementing authentication and management

--- service/module/authentication/authentication-domain/src/main/kotlin/com/thomas/authentication/domain/properties/AuthenticationProperties.kt
+++ service/module/authentication/authentication-domain/src/main/kotlin/com/thomas/authentication/domain/properties/AuthenticationProperties.kt
@@ -1,8 +1,8 @@
 package com.thomas.authentication.domain.properties
 
 data class AuthenticationProperties(
-    val accessDurationSeconds: Long = 0,
-    val refreshDurationSeconds: Long = 0,
-    val resetDurationSeconds: Long = 0,
-    val minimumPasswordLength: Int = 0,
+    val accessDurationSeconds: Long,
+    val refreshDurationSeconds: Long,
+    val resetDurationSeconds: Long,
+    val minimumPasswordLength: Int,
 )
